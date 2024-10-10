5db406223606d876014bc824e614d105a2ced3c8
Manar ELhabbal <manarelhabbal619@gmail.com>
2024-10-11 01:34:54 +0300

    Create Madlibs Game.py

---
+++ Madlibs Game.py
@@ -0,0 +1,16 @@
+#Madlibs game
+#word game where you create a story
+#by filling in blanks with random words
+
+noun=input("Enter a noun (person,place,thing):")
+
+verb=input("Enter a verb ending with 'ing' :")
+
+adj=input("Enter an adjective (description) :")
+
+
+
+print(f"Today i went to a/an {adj} zoo .")
+print(f"In an exhibit , I saw a {noun}.")
+print(f"{noun} was {adj} and {verb} ")
+print(f"i was {adj}!")
